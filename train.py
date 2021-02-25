358996ece074d83974f162d0b263c4ed3118e268
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2021-02-25 22:06:54 +0200

    integrate w&b

--- train.py
+++ train.py
@@ -1,52 +1,32 @@
-import torch
-import gym
 import matplotlib.pyplot as plt
 from matplotlib import animation
-import cv2
 import numpy as np
-import argparse
+import gym
+import torch
 import pickle
 import os
 import time
-from gym_minigrid.wrappers import ImgObsWrapper, RGBImgPartialObsWrapper
+import random
 
-should_record = False
+import gym_minigrid.envs
+from gym_minigrid.wrappers import ImgObsWrapper, RGBImgPartialObsWrapper
+import wandb
+import click
+
+from xvfbwrapper import Xvfb
+
 recording = {
     'episodes': []
 }
+logs = {
+    'episode_durations': [],
+    'episode_returns': []
+}
 
-"""
-Ensure you have imagemagick installed with 
-sudo apt-get install imagemagick
-
-Taken from https://gist.github.com/botforge/64cbb71780e6208172bbf03cd9293553
-"""
-def save_frames_as_gif(frames, path='./gifs', filename='gym_animation.gif'):
-    #Mess with this to change frame size
-    dpi = 72.
-    plt.figure(figsize=(frames[0].shape[1] / dpi, frames[0].shape[0] / dpi), dpi=dpi)
-
-    patch = plt.imshow(frames[0])
-    plt.axis('off')
-
-    def animate(i):
-        patch.set_data(frames[i])
-
-    anim = animation.FuncAnimation(plt.gcf(), animate, frames = len(frames), interval=50)
-    os.makedirs(path, exist_ok=True)
-    anim.save(os.path.join(path, filename), writer='imagemagick', fps=30)
-
-def step(env, action):
-    obs, reward, done, info = env.step(action)
-
-    if reward != 0:
-        print(f'step: {info["step"]}, reward: {reward}')
-
-    if should_record:
-        step = (obs, action, reward, done)
-        recording['episodes'][i_episode].append(step)
-
-    return done
+'''According to: https://docs.wandb.ai/library/log#images-and-overlays'''
+def frames_to_video(frames, fps=24):
+    stacked_frames = np.array(frames).transpose((0, 3, 1, 2)) # (h,w,c) -> (t,c,h,w)
+    return wandb.Video(stacked_frames, fps=fps, format="gif")
 
 def save_experience():
     print('Saving experience...')
@@ -55,65 +35,98 @@ def save_experience():
     with open(recording_name, 'wb') as f:
         pickle.dump(recording, f)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--env",
-        help="gym environment to load",
-        default='MiniGrid-Triggers-3x3-v0'
-    )
-    parser.add_argument(
-        "--seed",
-        type=int,
-        help="random seed to generate the environment with",
-        default=-1
-    )
-    parser.add_argument(
-        "--record",
-        help="record agent experience",
-        default=False,
-        action='store_true'
-    )
-    parser.add_argument(
-        "--gifs",
-        help="how often to save a gif of agent completing an episode",
-        default=-1, # -1 is never
-    )
-    parser.add_argument(
-        "--episodes",
-        help="how many episodes to play",
-        default=100,
-    )
-    args = parser.parse_args()
+@click.command()
+@click.option('--env_name', type=str, default='MiniGrid-Triggers-3x3-v0')
+@click.option('--seed', type=int, default=42, help='random seed used')
+@click.option('--log_frequency', type=int, default=5e1, help='logging frequency in episodes')
+@click.option('--episodes', type=int, default=1e3, help='number of episodes to train for', required=True)
+@click.option('--learning_rate', type=float, default=1e-4, help='goal learning rate')
+@click.option('--buffer_size', default=1e5, type=int, help='size of the replay buffer')
+@click.option('--batch_size', default=32, help='size of the replay buffer before training')
+@click.option('--record', is_flag=True, required=True)
+def train(env_name, seed, log_frequency, episodes, 
+          learning_rate, buffer_size, batch_size, record):
 
-    should_record = args.record
-    env_name = args.env
-    n_episodes = int(args.episodes)
-    gifs_frequency = int(args.gifs)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    experiment = f'{env_name}_s{seed}_ep{episodes}_lr{learning_rate}_buff{buffer_size}_rec{record}'
+
+    wandb.login()
+    wandb.init(project='attentional_fw_baselines',
+               entity='ut-rl-credit',
+               name=experiment,
+               notes="mock initial run without DQN to test w&b",
+            #    tags=["baseline", "paper1"],
+               config=dict(
+                   env_name=env_name,
+                   seed=seed,
+                   log_frequency=log_frequency,
+                   episodes=episodes,
+                   learning_rate=learning_rate,
+                   buffer_size=buffer_size,
+                   batch_size=batch_size,
+                   record=record,
+               ))
+    # Upload models at the end of training
+    wandb.save(os.path.join(wandb.run.dir, "*.pt"))
+    wandb.save(os.path.join(wandb.run.dir, "recordings", "*.pt"))
+
+    torch.cuda.manual_seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     env = gym.make(env_name)
-    env = ImgObsWrapper(RGBImgPartialObsWrapper(env))
+    if 'MiniGrid' in env_name: # support non-MiniGrid environments
+        env = ImgObsWrapper(RGBImgPartialObsWrapper(env))
 
     obs = env.reset()
-    print(obs.shape)
-    try:
-        for i_episode in range(n_episodes):
+
+    print('Mock Training...')
+    with Xvfb() as xvfb:
+        for i_episode in range(episodes):
             obs = env.reset()
             recording['episodes'].append([])
-            frames = []
+            frames = [env.render(mode='rgb_array')]
+            rewards = []
 
             while True:
-                frames.append(env.render(mode='rgb_array'))
                 action = env.action_space.sample()
-                done = step(env, action)
+                obs, reward, done, info = env.step(action)
+                frames.append(env.render(mode='rgb_array'))
+
+                rewards.append(reward)
+
+                if should_record:
+                    step = (obs, action, reward, done)
+                    recording['episodes'][i_episode].append(step)
                 
                 if done:
-                    if gifs_frequency > 0 and i_episode % gifs_frequency == 0
-                        save_frames_as_gif(frames, filename=f'episode_{i_episode}.gif')
+                    logs['episode_returns'].append(sum(rewards))
+                    logs['episode_durations'].append(len(rewards))
+                    rewards = []
                     break
+
+            if (i_episode % log_frequency) == 0:
+                wandb.log({
+                    'avg_episode_duration': np.mean(logs['episode_durations']),
+                    'avg_episode_return': np.mean(logs['episode_returns']),
+                    'video': frames_to_video(frames),
+                    'episode': i_episode,
+                }, )
+                logs['episode_durations'] = []
+                logs['episode_returns'] = []
                 
-    except KeyboardInterrupt:
         env.close()
 
     if should_record:
         save_experience()
+
+
+if __name__ == '__main__':
+    train()
+    
+    
