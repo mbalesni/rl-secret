e33a30856fea504c455d20b328fce9590d3769de
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2021-02-28 23:10:56 +0200

    implement DQN

---
+++ dqn.py
@@ -0,0 +1,255 @@
+# adopted from https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+
+import os
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from PIL import Image
+import sys
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from timing import Timing
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQN(nn.Module):
+
+    def __init__(self, h, w, outputs):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 8, kernel_size=3, stride=2)
+        self.bn1 = nn.BatchNorm2d(8)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(16)
+
+        # Number of Linear input connections depends on output of conv2d layers
+        # and therefore the input image size, so compute it.
+        def conv2d_size_out(size, kernel_size = 3, stride = 1):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w, stride=2)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h, stride=2)))
+        linear_input_size = convw * convh * 16
+
+        self.head = nn.Linear(linear_input_size, outputs)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.reshape(x.size(0), -1))
+
+class RandomActor:
+
+    def __init__(self, screen_height, screen_width, n_channels, n_actions, buffer_size=5_000):
+        self.n_actions = n_actions
+        self.input_shape = (screen_height, screen_width, n_channels)
+
+        self.memory = ReplayMemory(buffer_size)
+        self.state = self.format_observation(np.zeros(self.input_shape))
+        self.last_observation = torch.clone(self.state)
+
+    def format_observation(self, screen):
+        return torch.from_numpy(screen.transpose((2, 0, 1))).unsqueeze(0).to(device, dtype=torch.float)
+
+    def new_episode(self, observation):
+        self.state = self.format_observation(np.zeros(self.input_shape))
+        self.last_observation = self.format_observation(observation)
+
+    def step(self, env):
+
+        # Select and perform an action
+        with Timing(timing, 'time_choose_act'):
+            action = torch.tensor([[random.randrange(self.n_actions)]], device=device, dtype=torch.long)
+
+        with Timing(timing, 'time_perform_act'):
+            raw_observation, reward, done, info = env.step(action.item())
+
+        with Timing(timing, 'time_format_obs'):
+            observation = self.format_observation(raw_observation)
+            reward = torch.tensor([reward], device=device)
+
+        if not done:
+            next_state = observation - self.last_observation
+        else:
+            next_state = None
+
+        # Store the transition in memory
+        self.memory.push(self.state, action, next_state, reward)
+
+        self.state = next_state
+        self.last_observation = observation
+
+        return raw_observation, reward, done, info, action
+
+
+class DQNActor:
+
+    def __init__(self, screen_height, screen_width, n_channels, n_actions, eps_start=1,
+                 eps_end=0.01, eps_test=0.001, eps_decay=250_000, eps_const=None, gamma=0.999, 
+                 batch_size=32, target_update=2000, learning_rate=0.00025, buffer_size=1_000_000):
+        self.n_actions = n_actions
+        self.gamma = gamma
+        if eps_const is None:
+            self.eps_start = eps_start
+            self.eps_end = eps_end
+            self.eps_decay = eps_decay
+        self.eps_const = eps_const
+        self.batch_size = batch_size
+        self.target_update = target_update
+
+        self.input_shape = (screen_height, screen_width, n_channels)
+        self.policy_net = DQN(screen_height, screen_width, n_actions).to(device)
+        self.target_net = DQN(screen_height, screen_width, n_actions).to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
+
+        self.optimizer = optim.RMSprop(self.policy_net.parameters(), lr=learning_rate)
+        self.memory = ReplayMemory(buffer_size)
+
+        self.steps_done = 0
+
+        self.state = self.format_observation(np.zeros(self.input_shape))
+        self.last_observation = torch.clone(self.state)
+
+    # Reshape to torch order CHW and add a batch dimension (BCHW)
+    def format_observation(self, screen):
+        return torch.from_numpy(screen.transpose((2, 0, 1))).unsqueeze(0).to(device, dtype=torch.float)
+
+    def select_action(self, observation):
+        sample = random.random()
+        if self.eps_const:
+            eps_threshold = self.eps_const
+        else:
+            eps_threshold = self.eps_start - self.steps_done * (self.eps_start - self.eps_end) / self.eps_decay
+            eps_threshold = max(self.eps_end, eps_threshold) # make sure we don't go lower than eps_endd
+        self.steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                # t.max(1) will return largest column value of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                return self.policy_net(observation).max(1)[1].view(1, 1)
+        else:
+            return torch.tensor([[random.randrange(self.n_actions)]], device=device, dtype=torch.long)
+
+    def optimize_model(self, i_episode):
+        if len(self.memory) < self.batch_size:
+            return
+        transitions = self.memory.sample(self.batch_size)
+
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), device=device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                                    if s is not None])
+
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.batch_size, device=device)
+        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        # Compute Huber loss
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.policy_net.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
+
+        if self.steps_done % self.target_update == 0:
+            self.target_net.load_state_dict(self.policy_net.state_dict())
+
+    def new_episode(self, observation):
+        self.state = self.format_observation(np.zeros(self.input_shape))
+        self.last_observation = self.format_observation(observation)
+
+    def step(self, env, i_episode, timing):
+
+        # Select and perform an action
+        with Timing(timing, 'time_choose_act'):
+            action = self.select_action(self.state)
+
+        with Timing(timing, 'time_perform_act'):
+            raw_observation, reward, done, info = env.step(action.item())
+
+        with Timing(timing, 'time_format_obs'):
+            observation = self.format_observation(raw_observation)
+            reward = torch.tensor([reward], device=device)
+
+        if not done:
+            next_state = observation - self.last_observation
+        else:
+            next_state = None
+
+        # Store the transition in memory
+        self.memory.push(self.state, action, next_state, reward)
+
+        # Move to the next state
+        self.state = next_state
+        self.last_observation = observation
+
+
+        # Perform one step of the optimization (on the target network)
+        with Timing(timing, 'time_optimize_model'):
+            self.optimize_model(i_episode)
+
+        return raw_observation, reward, done, info, action
+
--- train.py
+++ train.py
@@ -3,17 +3,23 @@ from matplotlib import animation
 import numpy as np
 import gym
 import torch
+import click
 import pickle
 import os
 import time
 import random
+from xvfbwrapper import Xvfb
+import sys
+
+import wandb
+from itertools import count
+from timing import Timing
+from tqdm import tqdm
 
 import gym_minigrid.envs
 from gym_minigrid.wrappers import ImgObsWrapper, RGBImgPartialObsWrapper
-import wandb
-import click
+from dqn import DQNActor, RandomActor
 
-from xvfbwrapper import Xvfb
 
 recording = {
     'episodes': []
@@ -24,8 +30,8 @@ logs = {
 }
 
 '''According to: https://docs.wandb.ai/library/log#images-and-overlays'''
-def frames_to_video(frames, fps=24):
-    stacked_frames = np.array(frames).transpose((0, 3, 1, 2)) # (h,w,c) -> (t,c,h,w)
+def frames_to_video(frames, fps=15):
+    stacked_frames = np.array(frames).transpose((0, 3, 1, 2)) # (h,w,c) -> (t,c,w,h)
     return wandb.Video(stacked_frames, fps=fps, format="gif")
 
 def save_experience():
@@ -39,38 +45,45 @@ def save_experience():
 @click.option('--env_name', type=str, default='MiniGrid-Triggers-3x3-v0')
 @click.option('--seed', type=int, default=42, help='random seed used')
 @click.option('--log_frequency', type=int, default=5e1, help='logging frequency in episodes')
-@click.option('--episodes', type=int, default=1e3, help='number of episodes to train for', required=True)
-@click.option('--learning_rate', type=float, default=1e-4, help='goal learning rate')
-@click.option('--buffer_size', default=1e5, type=int, help='size of the replay buffer')
-@click.option('--batch_size', default=32, help='size of the replay buffer before training')
-@click.option('--record', is_flag=True, required=True)
+@click.option('--episodes', type=int, help='number of episodes to train for', required=True)
+@click.option('--learning-rate', type=float, default=1e-1, help='goal learning rate')
+@click.option('--buffer_size', default=5e4, type=int, help='size of the replay buffer')
+@click.option('--buffer-prefill-steps', default=5e3, type=int, help='size of the prefilled replay buffer')
+@click.option('--batch-size', default=32, help='training batch size')
+@click.option('--record/--no-record', default=True)
+@click.option('--record-random', is_flag=True)
+@click.option('--use_wandb/--no-wandb', default=True)
 def train(env_name, seed, log_frequency, episodes, 
-          learning_rate, buffer_size, batch_size, record):
+          learning_rate, buffer_size, buffer_prefill_steps, 
+          batch_size, record, record_random, use_wandb):
 
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    experiment = f'{env_name}_s{seed}_ep{episodes}_lr{learning_rate}_buff{buffer_size}_rec{record}'
+    experiment = f'{env_name}_s{seed}_ep{episodes}_lr{learning_rate}_buff{buffer_size}_rec{record}_recrand{record_random}'
 
-    wandb.login()
-    wandb.init(project='attentional_fw_baselines',
-               entity='ut-rl-credit',
-               name=experiment,
-               notes="mock initial run without DQN to test w&b",
-            #    tags=["baseline", "paper1"],
-               config=dict(
-                   env_name=env_name,
-                   seed=seed,
-                   log_frequency=log_frequency,
-                   episodes=episodes,
-                   learning_rate=learning_rate,
-                   buffer_size=buffer_size,
-                   batch_size=batch_size,
-                   record=record,
-               ))
-    # Upload models at the end of training
-    wandb.save(os.path.join(wandb.run.dir, "*.pt"))
-    wandb.save(os.path.join(wandb.run.dir, "recordings", "*.pt"))
+    if use_wandb:
+        wandb.login()
+        wandb.init(project='attentional_fw_baselines',
+                entity='ut-rl-credit',
+                name=experiment,
+                notes="mock initial run without DQN to test w&b",
+                #    tags=["baseline", "paper1"],
+                config=dict(
+                    env_name=env_name,
+                    seed=seed,
+                    log_frequency=log_frequency,
+                    episodes=episodes,
+                    learning_rate=learning_rate,
+                    buffer_size=buffer_size,
+                    buffer_prefill_steps=buffer_prefill_steps,
+                    batch_size=batch_size,
+                    record=record,
+                    record_random=record_random,
+                ))
+        # Upload models at the end of training
+        wandb.save(os.path.join(wandb.run.dir, "*.pt"))
+        wandb.save(os.path.join(wandb.run.dir, "recordings", "*.pt"))
 
     torch.cuda.manual_seed(seed)
     torch.manual_seed(seed)
@@ -83,25 +96,60 @@ def train(env_name, seed, log_frequency, episodes,
     if 'MiniGrid' in env_name: # support non-MiniGrid environments
         env = ImgObsWrapper(RGBImgPartialObsWrapper(env))
 
-    obs = env.reset()
+    observation_shape = env.reset().shape
+    random_actor = RandomActor(*observation_shape, env.action_space.n, 
+                               buffer_size=buffer_size)
+    actor = DQNActor(*observation_shape, env.action_space.n, 
+                     learning_rate=learning_rate, buffer_size=buffer_size, 
+                     batch_size=batch_size)
+    if use_wandb:
+        wandb.watch(actor.policy_net)
 
-    print('Mock Training...')
+
     with Xvfb() as xvfb:
-        for i_episode in range(episodes):
-            obs = env.reset()
-            recording['episodes'].append([])
+
+        timing = dict()               
+        print('Filling buffer...')
+        i_episode = 0
+        for i_step in tqdm(range(buffer_prefill_steps)):
+            observation, reward, done, info, action = actor.step(env, i_episode, timing)
+
+            if record_random:
+                step = (observation, action, reward, done)
+                recording['episodes'][i_episode].append(step)
+            
+            if done:
+                i_episode += 1
+                observation = env.reset()
+                if record_random:
+                    recording['episodes'].append([])
+                actor.new_episode(observation)
+
+                if i_episode > 0 and (i_episode % log_frequency) == 0 and use_wandb:
+                    wandb.log({
+                        'buff_episode': i_episode,
+                        **{k: v['time'] / v['count'] for k, v in timing.items()}
+                    }, )
+                    timing = dict()
+
+        actor.memory = random_actor.memory
+        timing = dict()               
+        print('Training...')
+        for i_episode in tqdm(range(episodes)):
+            observation = env.reset()
+            actor.new_episode(observation)
+            if record:
+                recording['episodes'].append([])
             frames = [env.render(mode='rgb_array')]
             rewards = []
 
-            while True:
-                action = env.action_space.sample()
-                obs, reward, done, info = env.step(action)
+            for i_step in count():
+                observation, reward, done, info, action = actor.step(env, i_episode, timing)
                 frames.append(env.render(mode='rgb_array'))
-
                 rewards.append(reward)
 
-                if should_record:
-                    step = (obs, action, reward, done)
+                if record:
+                    step = (observation, action, reward, done)
                     recording['episodes'][i_episode].append(step)
                 
                 if done:
@@ -110,21 +158,24 @@ def train(env_name, seed, log_frequency, episodes,
                     rewards = []
                     break
 
-            if (i_episode % log_frequency) == 0:
+            if i_episode > 0 and (i_episode % log_frequency) == 0 and use_wandb:
+
                 wandb.log({
                     'avg_episode_duration': np.mean(logs['episode_durations']),
-                    'avg_episode_return': np.mean(logs['episode_returns']),
+                    'avg_episode_return': torch.mean(torch.tensor(logs['episode_returns'], device='cpu', dtype=torch.float)),
                     'video': frames_to_video(frames),
                     'episode': i_episode,
+                    **{k: v['time'] / v['count'] for k, v in timing.items()}
                 }, )
                 logs['episode_durations'] = []
                 logs['episode_returns'] = []
-                
-        env.close()
+                timing = dict()
 
-    if should_record:
+    if record:
         save_experience()
-
+    if use_wandb:   
+        wandb.finish()
+    env.close()
 
 if __name__ == '__main__':
     train()
