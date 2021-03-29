9720022a2d5d2b335708b9628c0b22757b8a934e
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2021-03-29 18:09:14 +0300

    fix lint issues

--- train_dqn.py
+++ train_dqn.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-from matplotlib import animation
 import numpy as np
 import gym
 import torch
@@ -9,14 +8,11 @@ import os
 import time
 import random
 from xvfbwrapper import Xvfb
-import sys
 
 import wandb
 from itertools import count
-from timing import Timing
 from tqdm import tqdm
 
-import gym_minigrid.envs
 from gym_minigrid.wrappers import ImgObsWrapper, RGBImgPartialObsWrapper, RGBImgObsWrapper
 from dqn import DQNActor, RandomActor
 
@@ -29,10 +25,14 @@ logs = {
     'episode_returns': []
 }
 
-'''According to: https://docs.wandb.ai/library/log#images-and-overlays'''
+
 def frames_to_video(frames, fps=15):
-    stacked_frames = np.array(frames).transpose((0, 3, 1, 2)) # (h,w,c) -> (t,c,w,h)
+    '''According to: https://docs.wandb.ai/library/log#images-and-overlays'''
+
+    stacked_frames = np.array(frames).transpose(
+        (0, 3, 1, 2))  # (h,w,c) -> (t,c,w,h)
     return wandb.Video(stacked_frames, fps=fps, format="gif")
+
 
 def save_experience(base_dir):
     print('Saving experience...')
@@ -42,6 +42,7 @@ def save_experience(base_dir):
     full_path = os.path.join(directory, recording_name)
     with open(full_path, 'wb') as f:
         pickle.dump(recording, f)
+
 
 @click.command()
 @click.option('--note', type=str, help='message to explain how is this run different')
@@ -61,9 +62,9 @@ def save_experience(base_dir):
 @click.option('--record/--no-record', default=True)
 @click.option('--record-random', is_flag=True)
 @click.option('--use-wandb/--no-wandb', default=True)
-def train(note, env_name, seed, log_frequency, episodes, 
-          learning_rate, gamma, eps_start, eps_end, eps_decay, 
-          buffer_size, buffer_prefill_steps, batch_size, 
+def train(note, env_name, seed, log_frequency, episodes,
+          learning_rate, gamma, eps_start, eps_end, eps_decay,
+          buffer_size, buffer_prefill_steps, batch_size,
           part_observ, record, record_random, use_wandb):
 
     torch.backends.cudnn.deterministic = True
@@ -71,27 +72,27 @@ def train(note, env_name, seed, log_frequency, episodes,
 
     wandb.login()
     wandb.init(project='attentional_fw_baselines',
-            entity='ut-rl-credit',
-            notes=note,
-            tags=["baseline", "SECRET", "DQN"],
-            mode='online' if use_wandb else 'disabled',
-            config=dict(
-                env_name=env_name,
-                seed=seed,
-                log_frequency=log_frequency,
-                episodes=episodes,
-                learning_rate=learning_rate,
-                gamma=gamma,
-                eps_start=eps_start, 
-                eps_end=eps_end, 
-                eps_decay=eps_decay,
-                buffer_size=buffer_size,
-                buffer_prefill_steps=buffer_prefill_steps,
-                batch_size=batch_size,
-                part_observ=part_observ,
-                record=record,
-                record_random=record_random,
-            ))
+               entity='ut-rl-credit',
+               notes=note,
+               tags=["baseline", "SECRET", "DQN"],
+               mode='online' if use_wandb else 'disabled',
+               config=dict(
+                   env_name=env_name,
+                   seed=seed,
+                   log_frequency=log_frequency,
+                   episodes=episodes,
+                   learning_rate=learning_rate,
+                   gamma=gamma,
+                   eps_start=eps_start,
+                   eps_end=eps_end,
+                   eps_decay=eps_decay,
+                   buffer_size=buffer_size,
+                   buffer_prefill_steps=buffer_prefill_steps,
+                   batch_size=batch_size,
+                   part_observ=part_observ,
+                   record=record,
+                   record_random=record_random,
+               ))
     # Upload models at the end of training
     wandb.save(os.path.join(wandb.run.dir, "*.pt"))
     wandb.save(os.path.join(wandb.run.dir, "recordings", "*.pt"))
@@ -101,10 +102,8 @@ def train(note, env_name, seed, log_frequency, episodes,
     np.random.seed(seed)
     random.seed(seed)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
     env = gym.make(env_name)
-    if 'MiniGrid' in env_name: # support non-MiniGrid environments
+    if 'MiniGrid' in env_name:  # support non-MiniGrid environments
         if part_observ:
             env = ImgObsWrapper(RGBImgPartialObsWrapper(env))
         else:
@@ -115,26 +114,27 @@ def train(note, env_name, seed, log_frequency, episodes,
     plt.imshow(observation)
     plt.savefig('obs.png')
     observation_shape = env.reset().shape
-    random_actor = RandomActor(*observation_shape, env.action_space.n, 
+    random_actor = RandomActor(*observation_shape, env.action_space.n,
                                buffer_size=buffer_size)
-    actor = DQNActor(*observation_shape, env.action_space.n, 
-                     learning_rate=learning_rate, buffer_size=buffer_size, 
-                     gamma=gamma, batch_size=batch_size, eps_start=eps_start, 
+    actor = DQNActor(*observation_shape, env.action_space.n,
+                     learning_rate=learning_rate, buffer_size=buffer_size,
+                     gamma=gamma, batch_size=batch_size, eps_start=eps_start,
                      eps_end=eps_end, eps_decay=eps_decay)
     wandb.watch(actor.policy_net)
 
-    with Xvfb() as xvfb:
+    with Xvfb():
 
-        timing = dict()               
+        timing = dict()
         print('Filling buffer...')
         i_episode = 0
         for i_step in tqdm(range(1, buffer_prefill_steps+1)):
-            observation, reward, done, info, action = random_actor.step(env, timing)
+            observation, reward, done, info, action = random_actor.step(
+                env, timing)
 
             if record_random:
                 step = (observation, action, reward, done)
                 recording['episodes'][i_episode].append(step)
-            
+
             if done:
                 i_episode += 1
                 observation = env.reset()
@@ -150,7 +150,7 @@ def train(note, env_name, seed, log_frequency, episodes,
                     timing = dict()
 
         actor.memory = random_actor.memory
-        timing = dict()               
+        timing = dict()
         print('Training...')
         for i_episode in tqdm(range(1, episodes+1)):
             observation = env.reset()
@@ -161,14 +161,15 @@ def train(note, env_name, seed, log_frequency, episodes,
             rewards = []
 
             for i_step in count():
-                observation, reward, done, info, action = actor.step(env, i_episode, timing)
+                observation, reward, done, info, action = actor.step(
+                    env, i_episode, timing)
                 frames.append(env.render(mode='rgb_array'))
                 rewards.append(reward)
 
                 if record:
                     step = (observation, action, reward, done)
                     recording['episodes'][i_episode].append(step)
-                
+
                 if done:
                     logs['episode_returns'].append(sum(rewards))
                     logs['episode_durations'].append(len(rewards))
@@ -190,18 +191,18 @@ def train(note, env_name, seed, log_frequency, episodes,
                 logs['episode_returns'] = []
                 timing = dict()
 
-                torch.save(actor.policy_net.state_dict(), os.path.join(wandb.run.dir, 'dqn_policy.pt'))
+                torch.save(actor.policy_net.state_dict(),
+                           os.path.join(wandb.run.dir, 'dqn_policy.pt'))
 
                 if record:
                     if use_wandb:
                         save_experience(wandb.run.dir)
                     else:
                         save_experience('recordings')
-            
+
     env.close()
-    wandb.finish()            
+    wandb.finish()
+
 
 if __name__ == '__main__':
     train()
-    
-    
